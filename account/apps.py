b828ec22f5772fb4f1f7908cc240c045d08593ad
shinkeonkim <singun11@kookmin.ac.kr>
2021-01-23 23:41:29 +0900

    feat: HNM-0050 기본 앱 세팅

---
+++ account/apps.py
@@ -0,0 +1,8 @@
+"""account(사용자 계정) 앱 설정 파일입니다."""
+from django.apps import AppConfig
+
+
+class AccountConfig(AppConfig):
+    """account 앱의 설정들을 정의합니다."""
+
+    name = "account"
---
+++ menu/apps.py
@@ -0,0 +1,8 @@
+"""menu(메뉴) 앱 설정 파일입니다."""
+from django.apps import AppConfig
+
+
+class MenuConfig(AppConfig):
+    """menu 앱의 설정들을 정의합니다."""
+
+    name = "menu"
