ab6b27019d70c8e700ace3e78e13f67c8c792c66
shinkeonkim <singun11@kookmin.ac.kr>
2021-02-20 15:36:41 +0900

    feat: HNM-0056 글 수정 기능

--- article/urls.py
+++ article/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 from .views import (
-    show, new, new_comment,
+    show, new, new_comment, edit, delete
 )
 
 app_name = 'article'
@@ -11,5 +11,7 @@ urlpatterns = [
     # path('', index, name="index"),
     path('<int:article_id>', show, name="show"),
     path('new/<int:board_id>', new, name="new"),
+    path('edit/<int:article_id>', edit, name="edit"),
+    path('delete/<int:article_id>', delete, name="delete"),
     path('<int:article_id>/comment/new/', new_comment, name="new_comment")
 ]
