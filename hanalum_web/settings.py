891d57c734d490653731232857baad809d107355
shinkeonkim <singun11@kookmin.ac.kr>
2021-03-01 00:25:24 +0900

    feat: HNM-0093 hashtag 초기 세팅

--- hanalum_web/settings.py
+++ hanalum_web/settings.py
@@ -168,6 +168,7 @@ INSTALLED_APPS = [
     "menu",
     "notice",
     "policy",
+    "hashtag",
     "history",
 ]
 
---
+++ hashtag/admin.py
@@ -0,0 +1,19 @@
+""" hashtag 어드민 사이트 설정 파일입니다. """
+from django.contrib import admin
+
+from .models import HashTag
+
+@admin.register(HashTag)
+class HashTagAdmin(admin.ModelAdmin):
+    """게시글 어드민 설정 클래스입니다."""
+    list_display = [
+        "tagged_type",
+        "tagged_id",
+        "tagged_object",
+        "created_at",
+        "updated_at"
+    ]
+    list_filter = [
+        "tagged_type",
+        "tagged_id",
+    ]
