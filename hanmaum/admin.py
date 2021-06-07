008d5f3bb37b723e2fb2aabb7f2b7adeea33c4d8
shinkeonkim <singun11@kookmin.ac.kr>
2021-06-08 03:59:04 +0900

    feat: 한마음 캐시 카운터 리셋 액션 추가

--- hanmaum/admin.py
+++ hanmaum/admin.py
@@ -2,17 +2,46 @@
 from django.contrib import admin
 
 from .models import HanmaumArticle
+from history.models import LikeActivity, ViewHistory
 
 
 @admin.register(HanmaumArticle)
 class HanmaumAdmin(admin.ModelAdmin):
     """ 한마음 어드민 설정 클래스입니다. """
+    actions = [
+        'reset_like_and_dislike_counter',
+        'reset_viewed_counter',
+    ]
+
     list_display = [
         'id',
         'title',
         'interviewer',
         'interviewee',
+        'viewed_count',
+        'like_count',
+        'dislike_count',
         'created_at',
         'updated_at',
     ]
     list_filter = ["interviewer"]
+
+
+    def reset_like_and_dislike_counter(modeladmin, request, queryset):
+        """좋아요/싫어요 캐시 카운터를 쿼리를 날려 다시 설정한다."""
+        articles = queryset
+
+        for article in articles:
+            article.like_count = LikeActivity.get_like_count(article)
+            article.dislike_count = LikeActivity.get_dislike_count(article)
+            article.save()
+    reset_like_and_dislike_counter.short_description = '좋아요/싫어요 카운터 재설정'
+
+    def reset_viewed_counter(modeladmin, request, queryset):
+        """조회수 캐시 카운터를 쿼리를 날려 다시 설정한다."""
+        articles = queryset
+
+        for article in articles:
+            article.viewed_count = ViewHistory.total_viewed_count(article)
+            article.save()
+    reset_viewed_counter.short_description = '조회수 카운터 재설정'
