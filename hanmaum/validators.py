a4c5d3e3f9b700a9e38c1cfff46f0ee349490860
shinkeonkim <singun11@kookmin.ac.kr>
2021-06-10 23:37:12 +0900

    feat: hanmaum 권한이 있는 유저만 한마음 글 작성할 수 있도록 페이지 추가

--- hanmaum/validators.py
+++ hanmaum/validators.py
@@ -8,11 +8,21 @@ class HanmaumArticlePermissionValidator():
     """Article views마다의 권한 제어 클래스"""
 
     @classmethod
+    def new(cls, current_user):
+        if not current_user or not current_user.is_authenticated:
+            raise NoPermissionException()
+        if not current_user.is_hanmaum_member:
+            raise NoPermissionException()
+        return True
+
+    @classmethod
     def show(cls, current_user, article_id):
         """show validator"""
 
         article = HanmaumArticle.objects.get(pk=article_id)
 
+        if current_user and current_user.is_authenticated and current_user.is_hanmaum_member:
+            return True 
         if article.status != 'p':
             raise NoPermissionException()
 
