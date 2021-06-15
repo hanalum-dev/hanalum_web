846e55c949869aefa63383e018c613efc8c63857
shinkeonkim <singun11@kookmin.ac.kr>
2021-06-15 22:11:29 +0900

    feat: JOHA 사이트 서브 분리

--- joha/urls.py
+++ joha/urls.py
@@ -2,11 +2,17 @@
 from django.urls import path
 
 from .views import (
-    index
+    index,
+    JohaReviewsView,
 )
 
 app_name = 'joha'
 
 urlpatterns = [
     path('', index, name="index"),
+    path('reviews/<int:review_id>', JohaReviewsView.show, name="reviews_show"),
+    path('reviews/apply', JohaReviewsView.apply, name="reviews_apply"),
+    path('reviews', JohaReviewsView.index, name="reviews_index"),
+    path('reviews/new', JohaReviewsView.new, name="reviews_new"),
+    path('reviews/edit/<int:review_id>', JohaReviewsView.edit, name="reviews_edit"),
 ]
--- joha/views.py
+++ joha/views.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.shortcuts import render
 from copy import deepcopy as dp
 
@@ -7,3 +8,42 @@ def index(request):
     response = dp(default_response)
  
     return render(request, 'joha/index.dj.html', response)
+
+
+class JohaReviewsView:
+    @classmethod
+    def index(cls, request):
+        response = dp(default_response)
+
+        return render(request, 'joha/reviews/index.dj.html', response)
+
+
+    def show(request):
+        response = dp(default_response)
+
+        return render(request, 'joha/reviews/show.dj.html', response)
+
+
+    @classmethod
+    def new(request):
+        response = dp(default_response)
+
+        return render(request, 'joha/reviews/new.dj.html', response)
+
+
+    @classmethod
+    def edit(request):
+        response = dp(default_response)
+
+        return render(request, 'joha/reviews/edit.dj.html', response)
+
+
+    @classmethod
+    def apply(cls, request):
+        response = dp(default_response)
+        
+        if request.method == 'POST':
+            return
+            
+        return render(request, 'joha/reviews/apply.dj.html', response)
+    
